cb7256faca0ec5375cfc66c863a917ed5e4f142a
Mikhail Kovbasenko <mkovbasenkofff@mail.ru>
2025-02-24 13:00:21 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -5,7 +5,12 @@ import { USER_LOCALSTORAGE_KEY } from '../const/localeStorage';
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-  },
+});
+
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+  }
+
+  return config;
 });
