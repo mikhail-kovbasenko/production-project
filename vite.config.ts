08e7e4f8d4c270ea010da869a05c1a60e54d76ea
Mikhail Kovbasenko <mkovbasenkofff@mail.ru>
2025-06-16 17:33:31 +0300

    update eslint rules

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,8 @@ export default defineConfig({
     __API__: JSON.stringify('http://localhost:8000'),
     __PROJECT__: JSON.stringify('frontend'),
   },
+  server: {
+    open: true,
+    port: 5173,
+  },
 });
