187caef3edc9ad44f309a94c93daf77989eaf799
Mikhail Kovbasenko <mkovbasenkofff@mail.ru>
2025-06-18 10:11:15 +0300

    upgrade webpack config

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -16,24 +16,17 @@ export function buildPlugins({
   apiUrl,
   project,
 }: BuildOptions): WebpackPluginInstance[] {
+  const isProd = !isDev;
+
   const plugins = [
     new HTMLWebpackPlugin({
       template: paths.html,
     }),
     new ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css',
-    }),
     new DefinePlugin({
       IS_DEV: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
       __PROJECT__: JSON.stringify(project),
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: paths.locales, to: paths.buildLocales },
-      ],
     }),
     // new CircularDependencyPlugin({
     //   exclude: /node_modules/,
@@ -54,6 +47,18 @@ export function buildPlugins({
     plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
     plugins.push(new ReactRefreshWebpackPlugin({ overlay: false }));
     plugins.push(new HotModuleReplacementPlugin());
+  }
+
+  if (isProd) {
+    plugins.push(new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }));
+    plugins.push(new CopyPlugin({
+      patterns: [
+        { from: paths.locales, to: paths.buildLocales },
+      ],
+    }));
   }
 
   return plugins;
