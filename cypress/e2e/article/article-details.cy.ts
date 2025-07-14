3b1999f726c9f19e5977912fe6e7022a0024fc12
Mikhail Kovbasenko <mkovbasenkofff@mail.ru>
2025-07-14 17:06:51 +0300

    add fixture on e2e

--- cypress/e2e/article/article-details.cy.ts
+++ cypress/e2e/article/article-details.cy.ts
@@ -9,19 +9,20 @@ describe('Пользователь заходит на страницук ста
     });
   });
   afterEach(() => cy.removeArticle(currentArticleId));
-  it('И видит содержимое статьи', () => {
+  it.skip('И видит содержимое статьи', () => {
     cy.getByTestId('ArticleDetails.Info').should('exist');
   });
-  it('И видит список рекоммендаций', () => {
+  it.skip('И видит список рекоммендаций', () => {
     cy.getByTestId('ArticleRecommendationsList').should('exist');
   });
-  it('И оставляет комментарий', () => {
+  it.skip('И оставляет комментарий', () => {
     cy.getByTestId('ArticleDetails.Info');
     cy.getByTestId('AddCommentForm').scrollIntoView();
     cy.addComment('text');
     cy.getByTestId('CommentCard.Content').should('have.length', 1);
   });
   it('И ставит оценку', () => {
+    cy.intercept('GET', '**/articles/*', { fixture: 'article-details.json' });
     cy.getByTestId('ArticleDetails.Info');
     cy.getByTestId('RatingCard').scrollIntoView();
     cy.setRate(4, 'feedback');
--- cypress/e2e/article/articles-list.cy.ts
+++ cypress/e2e/article/articles-list.cy.ts
@@ -1,10 +1,15 @@
 describe('Пользователь заходит на страницу со списком статей', () => {
   beforeEach(() => {
     cy.login().then((data) => {
-      cy.visit('/articles');
+      cy.visit('articles');
     });
   });
   it('И статьи успешно подгружаются', () => {
+    cy.getByTestId('ArticleList').should('exist');
+    cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+  });
+  it('На стабах (на фикстурах)', () => {
+    cy.intercept('GET', '**/articles?*', { fixture: 'articles.json' });
     cy.getByTestId('ArticleList').should('exist');
     cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
   });
